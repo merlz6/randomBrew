0358f06d143fc2af47ef6d9cf75b4eb8f8958fb5
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2019-03-31 22:04:47 -0400

    able to use the button to get a new random

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showRandom:true
+    }
+    this.toggleRandom = this.toggleRandom.bind(this)
+  }
 
+  toggleRandom () {
+    window.location.reload();
+  }
   render() {
     return (
       <div className="App">
         <h1> SophistiDrinker </h1>
+        <button onClick={this.toggleRandom} >New Random</button>
         <RandomBeer  />
+
+
 
       </div>
     );
@@ -82,52 +95,5 @@ class RandomBeer extends Component {
   }
 }
 
-// class BeerSearchForm extends Component {
-//   constructor(props){
-//     super(props);
-//
-//     this.state = {
-//       name:'',
-//       tagline:'',
-//       food_pairing:[],
-//       image_url:'',
-//       abv:''
-//     }
-//     function handleSearch(beer){
-//       let url = "https://api.punkapi.com/v2/beers/" + beer + ''
-//       fetch(url)
-//       .then(response => response.json()).then((data) => {
-//         console.log(data);
-//       })
-//     }
-//
-//   function handleSubmit(event){
-//       event.preventDefault();
-//       const beerName = event.target.name.value
-//       this.props.handleSubmit(beerName);
-//     }
-//   }
-//   render(){
-//     return (
-//       <div>
-//         <form onSubmit={this.handleSubmit}>
-//           <input
-//             name="name"
-//             type="text"
-//             placeholder="Enter Beer name for more info!"
-//             className="form-control"
-//           />
-//           <button type="submit" >Search </button>
-//         </form>
-//         {
-//           (this.state.name !== '')
-//           ?
-//           <p> {this.state.name} </p>
-//           :
-//           ''
-//         }
-//       </div>
-//     )
-//   }
-// }
+
 export default App;
